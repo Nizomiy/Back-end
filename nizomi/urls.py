f49d70fb65698c54e6ffdf605f6a18d77cbfda30
yusuf <valixonovyusuf01@gmail.com>
2025-08-22 18:28:49 +0500

    'update'

--- nizomi/urls.py
+++ nizomi/urls.py
@@ -32,5 +32,6 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', include('dashboard.urls')), 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
