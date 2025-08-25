cd464e72462e1f07d048b1a992b7477ee69473e2
AbdurrohmanJCom <tuckerticket@gmail.com>
2025-08-25 12:16:21 +0500

    x

--- nizomi/urls.py
+++ nizomi/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 
@@ -30,9 +31,15 @@ schema_view = get_schema_view(
     ),
     public=True,
 )
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('router/',include('api.urls'))
-
+    path('api/', include('api.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
+
+# Add media and static files serving for development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
